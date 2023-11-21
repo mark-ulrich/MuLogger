a0cc11febd990c5c66ef625e932c3b342956dded
Mark Ulrich <mark.ulrich.86@gmail.com>
2023-11-21 08:53:50 -0600

    Update to use log_level

--- MuLogger/ConsoleLogger.py
+++ MuLogger/ConsoleLogger.py
@@ -12,38 +12,39 @@ LOG_LEVEL_FATAL = 0x04
 LOG_LEVEL_DEBUG = 0x05
 
 COLOR_CODES = {
-    LOG_LEVEL_INFO: '\033[92m',
-    LOG_LEVEL_VERBOSE: '\033[93m',
-    LOG_LEVEL_WARNING: '\033[93m',
-    LOG_LEVEL_ERROR: '\033[91m',
-    LOG_LEVEL_FATAL: '\033[91m',
-    LOG_LEVEL_DEBUG: '\033[93m'
+    LOG_LEVEL_INFO: "\033[92m",
+    LOG_LEVEL_VERBOSE: "\033[93m",
+    LOG_LEVEL_WARNING: "\033[93m",
+    LOG_LEVEL_ERROR: "\033[91m",
+    LOG_LEVEL_FATAL: "\033[91m",
+    LOG_LEVEL_DEBUG: "\033[93m",
 }
 
 PREFIXES = {
-    LOG_LEVEL_INFO: '[+]',
-    LOG_LEVEL_VERBOSE: '[+]',
-    LOG_LEVEL_WARNING: '[!]',
-    LOG_LEVEL_ERROR: '[!]',
-    LOG_LEVEL_FATAL: '[!]',
-    LOG_LEVEL_DEBUG: '[D]'
+    LOG_LEVEL_INFO: "[+]",
+    LOG_LEVEL_VERBOSE: "[+]",
+    LOG_LEVEL_WARNING: "[!]",
+    LOG_LEVEL_ERROR: "[!]",
+    LOG_LEVEL_FATAL: "[!]",
+    LOG_LEVEL_DEBUG: "[D]",
 }
 
 # TODO: Find a better way than just exposing this to the importing module
 log_to_file: bool = False
 
-log_file: str = 'log.txt'
+log_file: str = "log.txt"
 log_level: int = LOG_LEVEL_INFO
 log_use_colors: bool = True
 
 
 # TODO: DRY this code up
 
+
 def generate_log_str(msg: str, level: int, use_colors: bool = False) -> str:
     """Generate log string"""
-    log_str = f'{PREFIXES[level]} {msg}'
+    log_str = f"{PREFIXES[level]} {msg}"
     if use_colors:
-        log_str = f'\033{COLOR_CODES[level]}{log_str}\033[0m'
+        log_str = f"\033{COLOR_CODES[level]}{log_str}\033[0m"
     return log_str
 
 
@@ -55,8 +56,11 @@ def set_log_level(level: int) -> None:
 
 def log(level: int, msg: str) -> None:
     """Log message"""
+    global log_level
+    if level > log_level:
+        return
     if log_to_file:
-        with open(log_file, 'a') as f:
+        with open(log_file, "a") as f:
             f.write(generate_log_str(msg, LOG_LEVEL_INFO))
     else:
         print(generate_log_str(msg, level, log_use_colors))
